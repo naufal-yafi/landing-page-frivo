f27b1f27d61217fbe3e774a30f0bf03ad4265b2d
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2023-01-17 09:26:38 +0700

    create custom font and color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
+    colors: {
+      'neutral-900': '#343A40',
+      'neutral-50': '#F3F7F9',
+      'blue-500': '#309AFC',
+      'blue-100': '#CCE5F9',
+      'orange-500': '#EE831F',
+      'orange-100': '#EBDCCB',
+      'green-500': '#11990F',
+      'green-100': '#C5E5CA'
+    },
+    fontFamily: {
+      'inter-reguler': ['Inter-Reguler', 'sans-serif'],
+      'inter-medium': ['Inter-Medium', 'sans-serif'],
+      'inter-bold': ['Inter-Bold', 'sans-serif'],
+      'montserrat-bold': ['Montserrat-Bold', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
